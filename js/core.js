1f03fdfa3091a395bf6c4386716ed431aafc9528
Asylniet <71657767+Asylniet@users.noreply.github.com>
2020-10-03 09:29:36 +0600

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -3,14 +3,19 @@ $(function () {
 });
 
 $(function () {
-	$("#footer").load("/footer.html");
- });
+  $("#footer").load("/footer.html");
+});
 
- $("a[href*=#]").on("click", function(e){
-	var anchor = $(this);
-	$('html, body').stop().animate({
-		scrollTop: $(anchor.attr('href')).offset().top
-	}, 1200);
-	e.preventDefault();
-	return false;
-	});
+$("a[href*=#]").on("click", function (e) {
+  var anchor = $(this);
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(anchor.attr("href")).offset().top - 82,
+      },
+      1200
+    );
+  e.preventDefault();
+  return false;
+});
